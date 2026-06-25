e49ea0f2e674b46694547d348ad88d3901730ea9
sheraz <sheraz.asifali@gmail.com>
2026-06-25 11:13:55 +0100

    Initial Django project setup

---
+++ min_vid/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for min_vid project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'min_vid.settings')
+
+application = get_wsgi_application()
